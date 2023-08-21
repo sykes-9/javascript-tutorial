041b81c9958743b8b92f7ceb902f1e1aa92eba73
sykes-9 <sykessackitey9@gmail.com>
2023-08-21 09:12:56 +0000

    covered and learned Objects ie updating it's properties, adding new properties, deleting, using objects for lookups, testing objects for properties, accesing nested objects, accesing nested arrays, building a record collection, keep a copy of the collection for test and iterate while loop

--- index.js
+++ index.js
@@ -287,3 +287,155 @@ var entreeValue = testObj["an entree"];
 var drinkValue = testObj['the drink'];
 
 console.log(testObj["my side"]);
+
+// Updating Object Properties
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails":1,
+    "friends":["freeCodeCamp Campers"]    
+};
+
+myDog.name = "Happy Coder";
+console.log(myDog.name);
+
+// Add New Properties to an Object
+myDog['bark'] = "woof!"
+console.log(myDog)
+
+
+// Delete Properties from an object
+delete myDog.tails;
+console.log(myDog);
+
+// Using Objects for Lookups
+function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie":"Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    };
+    result = lookup[val];
+    return result || val +'is not a valid code.';
+}
+
+console.log(phoneticLookup("bravo"));
+
+// testing objects for Properties
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp)){
+        return myObj[checkProp]
+    }else {
+        return "Not Found";
+    }
+}
+console.log(checkObj("shito"));
+
+// Accessing Nested Objects
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+    },
+    "outside": {
+        "trunk": "jack"
+     }
+    }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"];//NB used a bracket notation for the property because of the spacing 
+console.log(gloveBoxContents)
+
+
+// Accessing Nested Arrays
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+var secondTree = myPlants[1].list[1];
+console.log(secondTree)
+
+// Record Collection
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "22468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+// Kepp a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+    if (value === "") {
+        delete collection[id][prop];
+    }else if (prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    }else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+
+// Alter values below to test your code
+console.log(updateRecords(5439, "artist", "ABBA"));
+
+
+// Iterate with While loops
+var myArray = [];
+
+var i = 0;
+while(i < 5) {
+    myArray.push(i);
+    i++;
+}
+console.log(myArray);
+
+
+// Iterate with For Loops
